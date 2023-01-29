b41ffff7134977c287805ca24c53ee075600863d
Baamki <sevenbumki@gmail.com>
2023-01-29 21:36:13 +0900

    숙제

--- src/main/java/com/duo/toSever/ToSeverApplication.java
+++ src/main/java/com/duo/toSever/ToSeverApplication.java
@@ -3,6 +3,7 @@ package com.duo.toSever;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 승훈이가 EC2 만들 때 까지 버티기
 @SpringBootApplication
 public class ToSeverApplication {
 
